11626554f947788201af7f9aab89d598d4e5fb81
gocisse <m.mcisse@icloud.com>
2022-06-25 12:38:19 +0200

    Adding apiHandler method in API to handle credit cards transaction

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -1,10 +1,75 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
+
+	"github.com/gocisse/ecom-site/internal/cards"
 )
 
+type apiPayload struct {
+	Amount   string `json:"amount"`
+	Currency string `json:"currency"`
+}
+
+type jsonResponse struct {
+	OK      bool   `json:"ok"`
+	Message string `json:"message"`
+	Content string `json:"content"`
+	ID      string `json:"id"`
+}
+
 func (app *application) apiHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hit the api endpoint")
+
+	var paylod apiPayload
+
+	err := json.NewDecoder(r.Body).Decode(&paylod)
+	if err != nil {
+		app.errorLog.Printf("Error decoding JSON: %s", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
+
+	okay := true
+
+	card := cards.Card{
+		Secret:   app.config.stripe.secret,
+		Key:      app.config.stripe.key,
+		Currency: paylod.Currency,
+	}
+
+	amount, err := strconv.Atoi(paylod.Amount)
+	if err != nil {
+		app.errorLog.Printf("Error converting amount: %s", err)
+	}
+
+	pi, msg, err := card.Charge(amount, paylod.Currency)
+	if err != nil {
+		app.errorLog.Printf("Error charging card: %s", err)
+		okay = false
+	}
+	if okay {
+
+		out, err := json.MarshalIndent(pi, "", " ")
+		if err != nil {
+			fmt.Println(err)
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(out)
+
+	} else {
+		var j = jsonResponse{
+			OK: false,
+			Message: msg,
+		}
+
+		out, err := json.MarshalIndent(j, "", " ")
+		if err != nil {
+			fmt.Println(err)
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(out)
+	}
+
 }
